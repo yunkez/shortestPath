3b77c6c583e9143c8b1138c9e7c67b6d446ab5a4
AlickLiu1 <springscenery.liu@hotmail.com>
2018-11-06 18:26:39 +0800

    heap

---
+++ dijkstra_minHeap.py
@@ -0,0 +1,28 @@
+import heapq
+from collections import defaultdict
+
+def dijkstra_heap(graph, initial):
+    visited = {initial: 0}
+    h = [(0, initial)]
+    path = {}
+
+    nodes = set(graph.nodes)
+
+    while nodes and h:
+        current_weight, min_node = heapq.heappop(h)
+        try:
+            while min_node not in nodes:
+                current_weight, min_node = heapq.heappop(h)
+        except IndexError:
+            break
+
+        nodes.remove(min_node)
+
+        for v in graph.edges[min_node]:
+            weight = current_weight + graph.distances[min_node, v]
+            if v not in visited or weight < visited[v]:
+                visited[v] = weight
+                heapq.heappush(h, (weight, v))
+                path[v] = min_node
+
+    return visited, path
